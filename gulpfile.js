4005819207b9a48c0ee5117ec9e5adc4c258f133
Cristi Galea <cristi.m.galea@gmail.com>
2015-07-15 01:11:02 +0300

    minor refactoring. added plain css support

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,49 @@
 var  gulp = require('gulp'),
      uglify = require('gulp-uglify'),
      sass = require('gulp-sass'),
-     plumber = require('gulp-plumber');
+     plumber = require('gulp-plumber'),
+     uglifycss = require('gulp-uglifycss');
 
 // Scripts Task
-// Uglifies
+// Uglifies JS
 gulp.task('scripts', function () {
-    gulp.src('scripts/*.js')
+    gulp.src('scripts/**/*.js')
         .pipe(plumber())
         .pipe(uglify())
         .pipe(gulp.dest('build/scripts'));
 });
 
-// Styles Task
-// Uglifies
-gulp.task('styles', function () {
+// CSS Styles Task
+// Uglifies CSS
+
+gulp.task('processCSS', function () {
+    gulp.src('styles/**/*.css')
+        .pipe(plumber())
+        .pipe(uglifycss())
+        .pipe(gulp.dest('build/styles'));
+});
+
+// SASS Styles Task
+// Uglifies SASS
+gulp.task('processSASS', function () {
     gulp.src('styles/**/*.scss')
         .pipe(plumber())
         .pipe(sass({
             outputStyle: 'compressed'
         }))
-        .pipe(gulp.dest('build/styles'))
+        .pipe(gulp.dest('build/styles'));
 });
+
+// Global Styles Task
+// Triggers the uglify of CSS and SASS. Can add further support for LESS etc.
+gulp.task('styles', ['processCSS', 'processSASS']);
 
 // Watch Task
-// Watches JS
+// Watches JS & CSS/SASS
 gulp.task('watch', function () {
-    gulp.watch('scripts/*.js', ['scripts']);
-    gulp.watch('styles/**/*.scss', ['styles']);
+    gulp.watch('scripts/**/*.js', ['scripts']);
+    gulp.watch(['styles/**/*.css', 'styles/**/*.scss'], ['styles']);
 });
 
+// Main task
 gulp.task('default', ['scripts', 'styles', 'watch']);
